05cc08d9a8e7b1f970ddde214e40ac0b1d72cc93
Andrey Ten <444ten@gmail.com>
2015-06-16 12:50:39 +0300

    Project sructure

---
+++ 444tenIOS/444tenIOS/TENAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TENAppDelegate.h
+//  444tenIOS
+//
+//  Created by Andrey Ten on 6/16/15.
+//  Copyright (c) 2015 Andrey Ten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TENAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 444tenIOS/444tenIOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  444tenIOS
+//
+//  Created by Andrey Ten on 6/16/15.
+//  Copyright (c) 2015 Andrey Ten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TENAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TENAppDelegate class]));
+    }
+}
