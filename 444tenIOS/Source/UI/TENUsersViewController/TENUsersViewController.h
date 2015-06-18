9a4fae46315c208571c7ba7dacbeed878533e77e
Andrey Ten <444ten@gmail.com>
2015-06-18 16:29:23 +0300

    TENUsersViewController -> initial commit

---
+++ 444tenIOS/Source/UI/TENUsersViewController/TENUsersViewController.h
@@ -0,0 +1,13 @@
+//
+//  TENUsersViewController.h
+//  444tenIOS
+//
+//  Created by Andrey Ten on 6/18/15.
+//  Copyright (c) 2015 Andrey Ten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TENUsersViewController : UIViewController
+
+@end
---
+++ 444tenIOS/Source/UI/TENUsersViewController/TENUsersViewController.m
@@ -0,0 +1,25 @@
+//
+//  TENUsersViewController.m
+//  444tenIOS
+//
+//  Created by Andrey Ten on 6/18/15.
+//  Copyright (c) 2015 Andrey Ten. All rights reserved.
+//
+
+#import "TENUsersViewController.h"
+
+@interface TENUsersViewController ()
+
+@end
+
+@implementation TENUsersViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
