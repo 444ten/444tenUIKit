cb64a9b1b5512ada2071043f653ce17f57a44b26
Andrey Ten <444ten@gmail.com>
2015-06-18 16:38:55 +0300

    TENUser -> initial commit

---
+++ 444tenIOS/Source/Models/TENUser/TENUser.h
@@ -0,0 +1,14 @@
+//
+//  TENUser.h
+//  444tenIOS
+//
+//  Created by Andrey Ten on 6/18/15.
+//  Copyright (c) 2015 Andrey Ten. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TENUser : NSObject
+@property (nonatomic, strong)   NSString    *name;
+
+@end
