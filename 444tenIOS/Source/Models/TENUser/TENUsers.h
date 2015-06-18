a627a5226f92e3b621cf9ec698c824ff0474b907
Andrey Ten <444ten@gmail.com>
2015-06-18 16:45:34 +0300

    TENUsers -> initial commit

---
+++ 444tenIOS/Source/Models/TENUser/TENUsers.h
@@ -0,0 +1,13 @@
+//
+//  TENUsers.h
+//  444tenIOS
+//
+//  Created by Andrey Ten on 6/18/15.
+//  Copyright (c) 2015 Andrey Ten. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TENUsers : NSObject
+
+@end
---
+++ 444tenIOS/Source/Models/TENUser/TENUsers.m
@@ -0,0 +1,13 @@
+//
+//  TENUsers.m
+//  444tenIOS
+//
+//  Created by Andrey Ten on 6/18/15.
+//  Copyright (c) 2015 Andrey Ten. All rights reserved.
+//
+
+#import "TENUsers.h"
+
+@implementation TENUsers
+
+@end
