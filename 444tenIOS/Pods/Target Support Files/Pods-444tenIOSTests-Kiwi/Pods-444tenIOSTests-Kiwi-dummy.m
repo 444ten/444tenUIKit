d07f349a7c292e6bf21766d47abe23bae1802491
Andrey Ten <444ten@gmail.com>
2015-06-24 14:29:30 +0300

    kiwi

---
+++ 444tenIOS/Pods/Target Support Files/Pods-444tenIOSTests-Kiwi/Pods-444tenIOSTests-Kiwi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_444tenIOSTests_Kiwi : NSObject
+@end
+@implementation PodsDummy_Pods_444tenIOSTests_Kiwi
+@end
---
+++ 444tenIOS/Pods/Target Support Files/Pods-444tenIOSTests/Pods-444tenIOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_444tenIOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_444tenIOSTests
+@end
