b4abaa177cd913e2353cc7a5f68040b4c023a87b
Andrey Ten <444ten@gmail.com>
2015-07-01 16:12:45 +0300

    TENImageModelDispatcher -> initial commit

---
+++ 444tenIOS/Source/Models/TENImageModel/Private/TENImageModelDispatcher.h
@@ -0,0 +1,13 @@
+//
+//  TENImageModelDispatcher.h
+//  444tenIOS
+//
+//  Created by Andrey Ten on 7/1/15.
+//  Copyright (c) 2015 Andrey Ten. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TENImageModelDispatcher : NSObject
+
+@end
---
+++ 444tenIOS/Source/Models/TENImageModel/Private/TENImageModelDispatcher.m
@@ -0,0 +1,13 @@
+//
+//  TENImageModelDispatcher.m
+//  444tenIOS
+//
+//  Created by Andrey Ten on 7/1/15.
+//  Copyright (c) 2015 Andrey Ten. All rights reserved.
+//
+
+#import "TENImageModelDispatcher.h"
+
+@implementation TENImageModelDispatcher
+
+@end
