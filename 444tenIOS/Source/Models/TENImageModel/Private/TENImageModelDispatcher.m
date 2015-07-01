513d32722475f526e5b0060a3c612c99aca4f95a
Andrey Ten <444ten@gmail.com>
2015-07-01 16:36:31 +0300

    TENImageModelDispatcher

--- 444tenIOS/Source/Models/TENImageModel/Private/TENImageModelDispatcher.m
+++ 444tenIOS/Source/Models/TENImageModel/Private/TENImageModelDispatcher.m
@@ -8,6 +8,61 @@
 
 #import "TENImageModelDispatcher.h"
 
+@interface TENImageModelDispatcher ()
+@property (nonatomic, strong) NSOperationQueue    *queue;
+
+- (void)initQueue;
+
+@end
+
 @implementation TENImageModelDispatcher
+
+#pragma mark -
+#pragma mark Class Methods
+
++ (instancetype)sharedDispatcher {
+    static id dispatcher = nil;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        dispatcher = [self new];
+    });
+
+    return dispatcher;
+}
+
+#pragma mark -
+#pragma mark Initializations and Deallocations
+
+- (void)dealloc {
+    self.queue = nil;
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        [self initQueue];
+    }
+    
+    return self;
+}
+
+- (void)initQueue {
+    NSOperationQueue *queue = [NSOperationQueue new];
+    queue.maxConcurrentOperationCount = 2;
+    
+    self.queue = queue;
+}
+
+#pragma mark -
+#pragma mark Accessors
+
+- (void)setQueue:(NSOperationQueue *)queue {
+    if (_queue != queue) {
+        [_queue cancelAllOperations];
+        
+        _queue = queue;
+    }
+}
 
 @end
