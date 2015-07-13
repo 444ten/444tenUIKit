a6fff4fe0c12c1392861a744ec1126bdaafa1f6b
Andrey Ten <444ten@gmail.com>
2015-07-13 13:26:36 +0300

    TENImage -> initial commit

---
+++ 444tenIOS/Source/Models/TENImage/TENImage.h
@@ -0,0 +1,13 @@
+//
+//  TENImage.h
+//  444tenIOS
+//
+//  Created by Andrey Ten on 7/13/15.
+//  Copyright (c) 2015 Andrey Ten. All rights reserved.
+//
+
+#import "TENModel.h"
+
+@interface TENImage : TENModel
+
+@end
---
+++ 444tenIOS/Source/Models/TENImage/TENImage.m
@@ -0,0 +1,13 @@
+//
+//  TENImage.m
+//  444tenIOS
+//
+//  Created by Andrey Ten on 7/13/15.
+//  Copyright (c) 2015 Andrey Ten. All rights reserved.
+//
+
+#import "TENImage.h"
+
+@implementation TENImage
+
+@end
