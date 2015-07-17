3033507fd37f6a605ee3029d3ab6515ff775fbff
Andrey Ten <444ten@gmail.com>
2015-07-17 18:20:37 +0300

    TENImageCaсhe -> initial commit

---
+++ 444tenIOS/Source/Library/Classes/TENImageCaсhe/TENImageCaсhe.h
@@ -0,0 +1,13 @@
+//
+//  TENImageCaсhe.h
+//  444tenIOS
+//
+//  Created by Andrey Ten on 7/17/15.
+//  Copyright (c) 2015 Andrey Ten. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TENImageCa_he : NSObject
+
+@end
---
+++ 444tenIOS/Source/Library/Classes/TENImageCaсhe/TENImageCaсhe.m
@@ -0,0 +1,13 @@
+//
+//  TENImageCaсhe.m
+//  444tenIOS
+//
+//  Created by Andrey Ten on 7/17/15.
+//  Copyright (c) 2015 Andrey Ten. All rights reserved.
+//
+
+#import "TENImageCaсhe.h"
+
+@implementation TENImageCa_he
+
+@end
